20f6daf24bfe1821bd88d6c327fe6ba7f55c0a45
Michael Harris <meh@lostsidewalk.com>
2023-10-16 12:01:47 -0500

    Minor refactoring throughout

--- peoria_dei_data/get_dei_data.py
+++ peoria_dei_data/get_dei_data.py
@@ -2,61 +2,53 @@ import json
 import os
 import re
 from urllib.parse import urlparse
-
 import pandas as pd
 import requests
 import usaddress
 from bs4 import BeautifulSoup
 
 
-def cleanup_location(r):
-    location = r['location']
-    if location is not None:
-        try:
-            return usaddress.parse(location)
-        except:
-            print(f'Error parsing location {location}')
+def cleanup_location(location):
+    try:
+        return usaddress.parse(location) if location else None
+    except Exception as e:
+        print(f'Error parsing location: {location}')
 
 
-def cleanup_certifications(r):
+def cleanup_certifications(certification_information):
     all_certs = []
-    certs = r['certification_information'].split('\n')
+    certs = certification_information.split('\n')
     for cert in certs:
         cert_parts = cert.split(' - ', 1)
-        cert_location = cert_parts[0]
-        cert_details = cert_parts[1].split(' expiring on ')
-        cert_name = cert_details[0]
-        cert_expiration = cert_details[1]
-        cert_info = {
-            "location": cert_location,
-            "name": cert_name,
-            "expiration": cert_expiration,
-        }
-        all_certs.append(cert_info)
+        if len(cert_parts) == 2:
+            cert_location, cert_info = cert_parts
+            cert_name, cert_expiration = cert_info.split(' expiring on ')
+            cert_info = {
+                "location": cert_location,
+                "name": cert_name,
+                "expiration": cert_expiration,
+            }
+            all_certs.append(cert_info)
     return all_certs
 
 
 def cleanup_records(records):
     for r in records:
-        # parse certification information
-        r['certification_information'] = cleanup_certifications(r)
-        # parse location information
-        r['location'] = cleanup_location(r)
+        r['certification_information'] = cleanup_certifications(r.get('certification_information', ''))
+        r['location'] = cleanup_location(r.get('location', ''))
 
 
 def parse_business_information(fieldset_node):
     bus_info = re.sub('\n{2,}', '\n', fieldset_node.text)
-    bus_info_list = [x for x in bus_info.split('\n') if x.find(':') > 0]
+    bus_info_list = [x for x in bus_info.split('\n') if ':' in x]
     bus_info_dict = {}
     for item in bus_info_list:
-        key = item[:item.find(': ')]
-        value = item[item.find(': ') + 2:]
-        if value == 'None' or value == '':
+        key, value = item.split(': ', 1)
+        if not value or value == 'None':
             bus_info_dict[key] = None
-            continue
-        if key == 'Email' or key == 'Website':
+        elif key in ['Email', 'Website']:
             bus_info_dict[key] = value.lower()
-        elif key == 'Fax' or key == 'Phone':
+        elif key in ['Fax', 'Phone']:
             new_value = re.sub(r'[^0-9]', '', value)
             bus_info_dict[key] = new_value
         else:
@@ -66,55 +58,49 @@ def parse_business_information(fieldset_node):
 
 def parse_table(rows):
     return_dict = {}
-
     for row in rows:
         fieldset_node = row.find('td').find('fieldset')
         legend_text = fieldset_node.find('legend').text
-
         if legend_text == 'Business Information':
             return_dict['business_information_raw'] = fieldset_node.text
             return_dict['business_information'] = parse_business_information(fieldset_node)
             return_dict['business_information_addl'] = fieldset_node.text[len(legend_text):]
             ownership_text_match = re.search('This is an? (.+)-Owned Business', fieldset_node.text)
-            if ownership_text_match is not None:
+            if ownership_text_match:
                 ownership_text = ownership_text_match.group(1)
                 return_dict['Ownership Text'] = ownership_text
-                return_dict['Owner African American'] = ownership_text.find('African American') >= 0
-                return_dict['Owner Hispanic'] = ownership_text.find('Hispanic') >= 0
-                return_dict['Owner Asian'] = ownership_text.find('Asian') >= 0
-                return_dict['Owner Veteran'] = ownership_text.find('Veteran') >= 0
-                return_dict['Owner Female'] = ownership_text.find('Female') >= 0
+                return_dict['Owner African American'] = 'African American' in ownership_text
+                return_dict['Owner Hispanic'] = 'Hispanic' in ownership_text
+                return_dict['Owner Asian'] = 'Asian' in ownership_text
+                return_dict['Owner Veteran'] = 'Veteran' in ownership_text
+                return_dict['Owner Female'] = 'Female' in ownership_text
         else:
             return_dict[legend_text] = fieldset_node.text[len(legend_text):]
 
     return_dict_flattened = {}
     for k, v in return_dict.items():
-        if type(v) == dict:
-            for k2, v2 in v.items():
-                return_dict_flattened[k2] = v2
+        if isinstance(v, dict):
+            return_dict_flattened.update(v)
         else:
             return_dict_flattened[k] = v
-
     return return_dict_flattened
 
 
 def get_html_table_rows(page_content):
     soup = BeautifulSoup(page_content, "html.parser")
     table = soup.find(name='table', attrs={'class': 'sftable'})
-
     for br in table.find_all("br"):
         br.replace_with("\n" + br.text)  # note, </br> tags can technically contain text
-
     rows = table.findAll(lambda tag: tag.name == 'tr')
-
     return rows
 
 
-def process_url(url, local_page_dir=None):
+def process_url(url, local_page_dir):
     assert local_page_dir is not None, 'Must provide local_page_dir'
     assert os.path.isdir(local_page_dir), f'{local_page_dir} does not exist (need to manually create)'
     page_id = url.split('seq=')[-1]
     local_file = f'{local_page_dir}/{page_id}.html'
+
     if os.path.exists(local_file):
         print('Reading contents from local file')
         with open(local_file, 'r') as f:
@@ -124,11 +110,11 @@ def process_url(url, local_page_dir=None):
         print('Reading contents from web')
         try:
             url_contents = requests.get(url).text
-            print(f'  Writing: ../data/scrape_run_20231009/pages/{page_id}.html')
-            with open(f'../data/scrape_run_20231009/pages/{page_id}.html', 'w') as f:
+            print(f'Writing: {local_file}')
+            with open(local_file, 'w') as f:
                 f.write(url_contents)
-        except:
-            print(f'Error getting data for {url}')
+        except Exception as e:
+            print(f'Error getting data for {url}: {str(e)}')
 
     return {'url_contents': url_contents, 'page_id': page_id}
 
@@ -136,8 +122,8 @@ def process_url(url, local_page_dir=None):
 def read_url_list(peap_list_url):
     parsed_url = urlparse(peap_list_url)
     base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-
     url_list = []
+
     try:
         peap_list_contents = requests.get(peap_list_url).text
         soup = BeautifulSoup(peap_list_contents, "html.parser")
@@ -153,8 +139,9 @@ def read_url_list(peap_list_url):
             if first_anchor is None:
                 continue
             url_list.append(base_url + first_anchor.attrs['href'])
-    except:
-        print(f'Error getting data from {peap_list_url}')
+    except Exception as e:
+        print(f'Error getting data from {peap_list_url}: {str(e)}')
+
     return url_list
 
 
